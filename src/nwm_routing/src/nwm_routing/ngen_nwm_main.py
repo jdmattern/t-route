dbaca1ddaf5db2729a636d27e260841d529e793b
john mattern <john.mattern@nwcal-apd-dev1.nwc.nws.noaa.gov>
2021-07-29 21:17:15 +0000

    updates with ngen_nwm_main to have t-route be called from ngen

--- src/nwm_routing/src/nwm_routing/ngen_nwm_main.py
+++ src/nwm_routing/src/nwm_routing/ngen_nwm_main.py
@@ -6,23 +6,28 @@ import pathlib
 import pandas as pd
 
 ## network and reach utilities
-import troute.nhd_network as nhd_network
-import troute.nhd_io as nhd_io
-import troute.nhd_network_utilities_v02 as nnu
-import build_tests  # TODO: Determine whether and how to incorporate this into setup.py
+#import troute.nhd_network as nhd_network
+#import troute.nhd_io as nhd_io
+#import troute.nhd_network_utilities_v02 as nnu
+#import build_tests  # TODO: Determine whether and how to incorporate this into setup.py
 import sys
+import os
 
-import troute.routing.diffusive_utils as diff_utils
+#https://github.com/googleapis/oauth2client/issues/642
+if not hasattr(sys, 'argv'):
+    sys.argv  = ['']
 
-from .input import _input_handler_v02, _input_handler_v03
-from .preprocess import (
-    nwm_network_preprocess,
-    nwm_initial_warmstate_preprocess,
-    nwm_forcing_preprocess,
-)
-from .output import nwm_output_generator
+#import troute.routing.diffusive_utils as diff_utils
 
-from troute.routing.compute import compute_nhd_routing_v02
+#from .input import _input_handler_v02, _input_handler_v03
+#from .preprocess import (
+#    nwm_network_preprocess,
+#    nwm_initial_warmstate_preprocess,
+#    nwm_forcing_preprocess,
+#)
+#from .output import nwm_output_generator
+
+#from troute.routing.compute import compute_nhd_routing_v02
 
 
 def set_paths(root_path):
@@ -30,25 +35,51 @@ def set_paths(root_path):
     framework_path = "../../../python_framework_v02"
     routing_path = "../../../python_routing_v02"
     network_utilities_path = "../../../python_framework_v02/troute"
+    #nwm_routing_path =  "."
 
 
     framework_path_full = os.path.join(root_path, framework_path)
     routing_path_full = os.path.join(root_path, routing_path)
     network_utilities_path_full = os.path.join(root_path, network_utilities_path)
-
+ 
     sys.path.append(framework_path_full)
     sys.path.append(routing_path_full)
     sys.path.append(network_utilities_path_full)
 
     import troute.nhd_network as nhd_network
     #import compute_nhd_routing_v02
-    #from troute.routing.compute import compute_nhd_routing_v02
+    from troute.routing.compute import compute_nhd_routing_v02
     #import mc_reach
 
     import troute.nhd_io as nhd_io
 
     import troute.nhd_network_utilities_v02 as nnu
 
+    #from troute.routing.compute import compute_nhd_routing_v02
+
+    #import nwm_routing.input as input1
+ 
+    from nwm_routing.input import _input_handler_v02, _input_handler_v03
+    from nwm_routing.preprocess import (
+        nwm_network_preprocess,
+        nwm_initial_warmstate_preprocess,
+        nwm_forcing_preprocess,
+    )
+    from nwm_routing.output import nwm_output_generator
+
+
+    #import input._input_handler_v02 as _input_handler_v02
+    #import nwm_routing.input._input_handler_v02 as _input_handler_v02
+
+    #from input import _input_handler_v02, _input_handler_v03
+    '''
+    from nwm_routing.preprocess import (
+        nwm_network_preprocess,
+        nwm_initial_warmstate_preprocess,
+        nwm_forcing_preprocess,
+    )
+    from nwm_routing.output import nwm_output_generator
+    '''
 
 
     #from preprocess import ngen_preprocess
@@ -322,12 +353,37 @@ def _handle_args_v02(argv):
     return parser.parse_args(argv)
 
 
+#def main_v02():
 def main_v02(argv):
 
-    print ("argv")
+    print ("argv222")
     print (argv)
     print (type(argv))
     print ("end argv")
+    #from nwm_routing.input import _input_handler_v02, _input_handler_v03
+
+    import troute.nhd_network as nhd_network
+    #import compute_nhd_routing_v02
+    from troute.routing.compute import compute_nhd_routing_v02
+    #import mc_reach
+
+    import troute.nhd_io as nhd_io
+
+    import troute.nhd_network_utilities_v02 as nnu
+
+    #from troute.routing.compute import compute_nhd_routing_v02
+
+    #import nwm_routing.input as input1
+ 
+    from nwm_routing.input import _input_handler_v02, _input_handler_v03
+    from nwm_routing.preprocess import (
+        nwm_network_preprocess,
+        nwm_initial_warmstate_preprocess,
+        nwm_forcing_preprocess,
+    )
+    from nwm_routing.output import nwm_output_generator
+
+
 
     args = _handle_args_v02(argv)
     (
@@ -343,12 +399,7 @@ def main_v02(argv):
         coastal_parameters,
     ) = _input_handler_v02(args)
 
-    print ("main_v0222")
-    print ("argv")
-    print (argv)
-    print (type(argv))
-    print ("end argv")
-
+    print ("main_v02")
 
 
     dt = run_parameters.get("dt", None)
